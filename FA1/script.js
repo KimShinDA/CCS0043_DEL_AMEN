e2eb237dbee1d7393af33c082c33b2208b3630ed
KimShinDA <delamenkimshin@gmail.com>
2025-04-28 23:22:05 +0800

    FA1 - DONE | 04/28/25

--- FA1/script.js
+++
@@ -1,6 +0,0 @@
-function toggleMode() {
-    const body = document.body;
-    const button = document.getElementById("toggle-button");
-    body.classList.toggle("dark-mode");
-    button.textContent = body.classList.contains("dark-mode") ? "Switch to Light Mode" : "Switch to Dark Mode";
-  }
